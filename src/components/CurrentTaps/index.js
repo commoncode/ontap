b751f683d8f20668b4c17c840f90caa17fc16cf9
Mark Reid <contact@markreid.com.au>
2016-10-04 22:02:30 +1100

    Update current tap info; remove no-service taps; put github link in footer

--- src/components/CurrentTaps/index.js
+++ src/components/CurrentTaps/index.js
@@ -10,24 +10,8 @@ const CurrentTapsComponent = props => (
     </header>
     <section className={classes(['on-tap-list'])}>
       <article className={classes(['tap'])}>
-        <div className={classes(['tap-name'])}>Left
+        <div className={classes(['tap-name'])}>Left Tap
         </div>
-        <div className={classes(['beer'])}>
-          <header>
-            <h2 className={classes(['beer-name'])}>Steam Ale</h2>
-            <h3 className={classes(['brewery-name'])}>Mountain Goat</h3>
-          </header>
-          <p className={classes(['notes'])}>
-            The Steam is a crisp, certified organic ale. We incorporate a slap of wheat malt in the grist make-up and ferment it cool. We use Cascade and Citra hops to give it a fresh, zippy finish. Great as the weather warms up.
-          </p>
-        </div>
-        <div className={classes(['meta'])}>
-          <p className={classes(['abv'])}>4.5%</p>
-        </div>
-      </article>
-
-      <article className={classes(['tap'])}>
-        <h2 className={classes(['tap-name'])}>Right</h2>
         <div className={classes(['beer'])}>
           <header>
             <h2 className={classes(['beer-name'])}>Saison</h2>
@@ -42,24 +26,25 @@ const CurrentTapsComponent = props => (
         </div>
       </article>
 
-      <article className={classes(['tap no-service'])}>
-        <h2 className={classes(['tap-name'])}>Middle</h2>
+      <article className={classes(['tap'])}>
+        <h2 className={classes(['tap-name'])}>Right Tap</h2>
         <div className={classes(['beer'])}>
           <header>
-            <h2 className={classes(['beer-name'])}>No Service</h2>
+            <h2 className={classes(['beer-name'])}>Steam Ale</h2>
+            <h3 className={classes(['brewery-name'])}>Mountain Goat</h3>
           </header>
+          <p className={classes(['notes'])}>
+            The Steam is a crisp, certified organic ale. We incorporate a slap of wheat malt in the grist make-up and ferment it cool. We use Cascade and Citra hops to give it a fresh, zippy finish. Great as the weather warms up.
+          </p>
         </div>
-      </article>
-
-      <article className={classes(['tap no-service'])}>
-        <h2 className={classes(['tap-name'])}>Green Fridge</h2>
-        <div className={classes(['beer'])}>
-          <header>
-            <h2 className={classes(['beer-name'])}>No Service</h2>
-          </header>
+        <div className={classes(['meta'])}>
+          <p className={classes(['abv'])}>4.5%</p>
         </div>
       </article>
     </section>
+    <footer className={classes(['footer'])}>
+      <a href="https://github.com/commoncode/ontap">github.com/commoncode/ontap</a>
+    </footer>
   </div>
 )
 
