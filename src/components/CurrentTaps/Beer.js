0444e0365b59679731056eac21600a0c9799cb93
mylsb <mitchell.stewart@mylsb.email>
2016-10-07 12:54:58 +1100

    Add beer component and calc standard drinks function

---
+++ src/components/CurrentTaps/Beer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import compose from 'recompose/compose';
+import withProps from 'recompose/withProps';
+
+import styles from './current-taps.css';
+
+
+function calcStandardDrinks(abv, litres=0.500) {
+  // http://www.alcohol.gov.au/internet/alcohol/publishing.nsf/content/standard
+  const ethanol = .789;
+  const result = litres * abv * ethanol;
+  return Math.round(result * 100) / 100;
+}
+
+const classes = classnames.bind(styles);
+
+const Beer = ({ abv, beerName, breweryName, notes, standardDrinks }) =>
+  <div className={classes(['beer'])}>
+    <header>
+      <div className={classes(['title'])}>
+        <h2 className={classes(['beer-name'])}>
+          {beerName}
+        </h2>
+        <h3 className={classes(['brewery-name'])}>
+          {breweryName}
+        </h3>
+      </div>
+      <p className={classes(['abv'])}>
+        {`${abv}%`}
+        <small className={classes(['std'])}>{` (${standardDrinks} SD)` } </small>
+      </p>
+    </header>
+    <p className={classes(['notes'])}>
+      {notes}
+    </p>
+  </div>
+
+const enhance = compose(
+  withProps(({ abv }) => ({
+    standardDrinks: calcStandardDrinks(abv),
+  })),
+);
+
+export default enhance(Beer);
