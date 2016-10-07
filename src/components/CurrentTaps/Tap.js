78b89b2d78b2985abe9ce7394b6af3043452aebd
Mitchell Stewart <mitchell.stewart@mylsb.email>
2016-10-07 15:26:06 +1100

    Add standard drinks (#11)
    * Add recompose
    
    * Add standard drinks style
    
    * Add beer component and calc standard drinks function
    
    * Separate Tap component and make it dryable
    
    * Update to pull out data and components

---
+++ src/components/CurrentTaps/Tap.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import branch from 'recompose/branch';
+import classnames from 'classnames/bind';
+import compose from 'recompose/compose';
+import renderComponent from 'recompose/renderComponent';
+import withProps from 'recompose/withProps';
+
+import Beer from './Beer';
+import styles from './current-taps.css';
+
+const classes = classnames.bind(styles);
+
+const Tap = ({ tap, ...props }) =>
+  <article className={classes(['tap'])}>
+    <div className={classes(['tap-name'])}>
+      {tap}
+    </div>
+    <div className={classes(['beer'])}>
+      <header>
+        <Beer {...props} />
+      </header>
+    </div>
+  </article>
+
+const Dry = ({ tap }) =>
+  <article className={classes(['tap', 'no-service'])}>
+    <div className={classes(['tap-name'])}>
+      {tap}
+    </div>
+    <div className={classes(['beer'])}>
+      <header>
+        <h2 className={classes(['beer-name'])}>No Service</h2>
+      </header>
+    </div>
+  </article>
+
+const dryable = compose(
+  withProps(({ beerName }) => ({
+    dry: !!beerName,
+  })),
+  branch(
+    props => !props.dry,
+    renderComponent(Dry),
+    Component => Component
+  ),
+);
+export default dryable(Tap);
