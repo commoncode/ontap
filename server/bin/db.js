6332a268564c10906d338c134d0052eeefb455a8
Mark Reid <contact@markreid.com.au>
2016-10-09 17:11:14 +1100

    add a basic CLI to interact with the db module

---
+++ server/bin/db.js
@@ -0,0 +1,25 @@
+/**
+ * bin/db
+ *
+ * load the db module and run a REPL.
+ * handy for when you just want to tool around
+ * with the db methods.
+ */
+
+const path = require('path');
+const repl = require('repl');
+
+// needs same config as index.js
+require('dotenv').config();
+require('app-module-path').addPath(path.join(__dirname, '../'));
+
+
+const db = require('models/index.js');
+
+const r = repl.start('> ');
+
+
+// add .db to the REPL context
+r.context = Object.assign(r.context, {
+  db,
+});
