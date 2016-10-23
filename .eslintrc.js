b54e9401b79a5ea6ea768383928a5c6e9da6a60a
Mark Reid <contact@markreid.com.au>
2016-10-23 13:00:59 +1100

    eslint - disable jsx extensions until i fix imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,13 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    "rules": {
+      // disable these two until the resolver is configured
+      // so it understands paths relative to __dirname
+      "import/no-extraneous-dependencies": 0,
+      "import/no-unresolved": 0,
+
+      "react/jsx-filename-extension": 0, // todo - get this enabled
+    }
 };
