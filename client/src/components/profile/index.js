1b1fe188b9a79d3efb8698598ae87e7893722b04
Mark Reid <contact@markreid.com.au>
2016-11-28 10:06:30 +1100

    Add a (poorly styled) sign out button

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -1,34 +1,54 @@
 import React from 'react';
-import classnames from 'classnames/bind';
 
-import styles from './profile.css';
 import * as propTypes from '../../proptypes/';
 
-const classes = classnames.bind(styles);
+class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showMenu: false,
+    };
 
-const Profile = (props) => {
-  // wait until it's fetched
-  if (!props.fetched) return null;
+    this.toggle = this.toggle.bind(this);
+  }
 
-  return (
-    <div className={classes(['profile'])}>
-      { props.data && props.data.id &&
-        <div>
-          <img
-            className={classes(['avatar'])}
-            src={`${props.data.avatar}?sz=32`}
-            alt={`Logged in as ${props.data.name}`}
-          />
-        </div>
-      }
+  toggle() {
+    this.setState({
+      showMenu: !this.state.showMenu,
+    });
+  }
 
-      { (!props.data || !props.data.id) &&
-        <div className={classes(['nobody'])}>
-          <a href="/login">Login with Google</a>
-        </div>
-      }
-    </div>
-  );
+  render() {
+    const { props } = this;
+    const { showMenu } = this.state;
+
+    // wait until it's fetched
+    if (!props.fetched) return null;
+
+    return (
+      <div className="profile">
+        { props.data && props.data.id &&
+          <div>
+            <img
+              className="profile-avatar"
+              src={`${props.data.avatar}?sz=32`}
+              alt={`Logged in as ${props.data.name}`}
+              onClick={this.toggle}
+            />
+            <div className={`profile-menu ${showMenu && 'show'}`}>
+              <a href="/logout">Sign Out</a>
+            </div>
+          </div>
+        }
+
+        { (!props.data || !props.data.id) &&
+          <div className="profile-nobody">
+            <a href="/login">Login with Google</a>
+          </div>
+        }
+      </div>
+    );
+  }
 };
 
 Profile.propTypes = propTypes.profile;
